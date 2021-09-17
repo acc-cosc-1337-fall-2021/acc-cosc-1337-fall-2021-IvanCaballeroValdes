4f077a4c4d8221b205334bc59385f722b03f1ed0
Github Username <youremail@example.com>
2021-09-17 16:40:33 -0500

    hw 2 expressions

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,4 +1,4 @@
-#include "expressions.h"
+#include "hwexpressions.h"
 
 //example
 int add_numbers(int num1, int num2)
@@ -7,7 +7,13 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
-
-
+int multiply(int tax_rates, int meal_amount)
+{
+	return tax_rates * meal_amount;
+}
+int multiply(int meal_amount, int tip_rate)
+{
+	return meal_amount * tip_rate;
+}
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,28 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
 
 //write namespace using statement for cout
-
+using std::cin;  using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount;
+	cout<< "type number: ";
+	cin>>meal_amount;
+
+	double tip_rate;
+	cout<<" enter tip rate ";
+	cin>>tip_rate;
+
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	get_sales_tax_amount(meal_amount);
+	get_tip_amount(tip_rate);
 
 	return 0;
 }
