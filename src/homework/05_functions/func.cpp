b0adaf3ab8c4be20aebf0796b497afe19fd8201e
IvanCaballeroValdes <89616317+IvanCaballeroValdes@users.noreply.github.com>
2021-10-26 20:52:16 -0500

    Add files via upload

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,42 @@
 //add include statements
-
+#include "func.h"
+using namespace std;
 //add function code here
+double get_gc_content(const string& dna)
+{
+    double count;
+    count = 0;
+    for(int i=0; i<dna.size(); i++)
+    {
+        if(dna[i]=='G' || dna[i]=='C')
+        {
+            count++;
+        }
+    }
+    return count/dna.size();
+}
+string reverse_string(string dna)
+{
+    int len=dna.size();
+    int n=len-1;
+    for(int i=0; i<(len/2); i++)
+    {
+        char temp=dna[i];
+        dna[i]=dna[n];
+        dna[n]-temp;
+        n=n-1;
+    }
+    return dna; 
+}
+string get_dna_complement(string dna)
+{
+    string complement;
+    int i;
+    char letter;
+    for(int i=0; i<dna.size(); i++)
+    {
+        letter=(dna[i]);
+        complement.append(letter);
+    }
+    return complement;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+using namespace std;
 /*
 use a vector of int with values 8, 4, 20, 88, 66, 99
 
@@ -9,6 +11,16 @@ Program continues until user decides to exit.
 */
 int main() 
 {
-
+	int choice = 0;
+	do
+	{
+		cout<<"Menu"<<endl;
+		cout<<"1. get_gc_content"<<endl;
+		cout<<"2. get_dna_complement"<<endl;
+		cout<<"Enter choice: ";
+		cin>>choice;
+		
+	} while ( choice ! = 3);
+	
 	return 0;
 }
