32561083347241f4bfeb25a1b30c17294dcf07b5
IvanCaballeroValdes <89616317+IvanCaballeroValdes@users.noreply.github.com>
2021-10-26 20:53:18 -0500

    Add files via upload

--- test/homework/05_functions/05_functions_tests.cpp
+++ test/homework/05_functions/05_functions_tests.cpp
@@ -5,3 +5,8 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify get_dna_complement");
+{
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+}
