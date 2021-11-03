c03bd9ff2f583e8840a9599b63382814d5884c28
IvanCaballeroValdes <89616317+IvanCaballeroValdes@users.noreply.github.com>
2021-11-03 14:08:32 -0500

    Add files via upload

--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -5,3 +5,19 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test get letter grade using if")
+{
+	assert(get_letter_grade_using_if(95) == 'A');
+	assert(get_letter_grade_using_if(85) == 'B');
+	assert(get_letter_grade_using_if(75) == 'C');
+	assert(get_letter_grade_using_if(65) == 'D');
+	assert(get_letter_grade_using_if(50) == 'F');
+}
+TEST_CASE("Test get letter grade using switch")
+{
+	assert(get_letter_grade_using_switch(95) == 'A');
+	assert(get_letter_grade_using_switch(85) == 'B');
+	assert(get_letter_grade_using_switch(75) == 'C');
+	assert(get_letter_grade_using_switch(65) == 'D');
+	assert(get_letter_grade_using_switch(50) == 'F');
+}
