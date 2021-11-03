b498baf971b78cf3129f1928edde2da5d796b024
IvanCaballeroValdes <89616317+IvanCaballeroValdes@users.noreply.github.com>
2021-11-03 14:06:25 -0500

    Add files via upload

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,71 @@
 //write include statement for decisions header
+#include "decisions.h"
 
 
 //Write code for function(s) code here
+char letter_a = 'A';
+char letter_b = 'B';
+char letter_c = 'C';
+char letter_d = 'D';
+char letter_f = 'F';
+string letter_grade;
+int grade;
+int num;
+
+string get_letter_grade_using_if(int grade)
+{
+    {
+
+        if (grade >= 90 && grade <= 100)
+        {
+            letter_grade = "A";
+
+        }
+        else if (grade >= 80 && grade <= 89)
+        {
+            letter_grade = "B";
+        }
+        else if (grade >= 70 && grade <= 79)
+        {
+            letter_grade = "C";
+        }
+        else if (grade >= 60 && grade <= 69)
+        {
+             letter_grade = "D";
+        }
+        else if (grade <= 59)
+        {
+            letter_grade = "F";
+        }
+        else 
+        {
+            return "number is out of range.";
+
+        }
+    }
+    return letter_grade;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    switch (grade)
+    {
+        case 90 ... 100:
+            return "A";
+            break;
+        case 80 ... 89:
+            return "B";
+            break;
+        case 70 ... 79:
+            return "C";
+            break;
+        case 60 ... 69:
+            return "D";
+            break;
+        case 0 ... 59:
+            return "F";
+            break;
+        default:
+            return "invalid number.";
+    }
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,42 @@
 //write include statements
+#include "decisions.h"
+#include "decisions.cpp"
+
+using std::cout; using std::cin;
+using std::string;
 
 int main() 
 {
-	return 0;
+    int options;
+    int grade;
+
+    cout<<"Welcome!\n"
+    "\nType 1 to get your grade using and if statement."<<
+    "\nType 2 to get your grade using a switch statement."<<
+    "\nType 3 to exit.\n"
+    "\nType number here: ";
+
+    cin>>options;
+
+
+    switch (options)
+    {
+        case 1:
+            cout<<"Enter grade: ";
+            cin>>grade;
+            cout<<"You got letter: "<<get_letter_grade_using_if(grade)<<"\n";
+            break;
+        case 2:
+            cout<<"Enter grade: ";
+            cin>>grade;
+            cout<<"You got letter: "<<get_letter_grade_using_switch(grade)<<"\n";
+            break;
+        case 3:
+            cout<<"\nThanks for coming!\n";
+            break;
+        default:
+            cout<<"\nThat's not a valid entry.\n";
+
+    }
+    return 0;
 }
