47ffb067234856dafc6ed8411ad8367a4f7133c7
IvanCaballeroValdes <89616317+IvanCaballeroValdes@users.noreply.github.com>
2021-11-06 16:43:03 -0500

    Add files via upload

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,29 @@
+#include "tic_tac_toe.h"
+
+using std::cin; using::cout; using std::string
 
 int main() 
 {
+	string first_player;
+	char choice;
+	TicTacToe game;
+	do
+	{
+		
+		cout<<"Enter first player: ";
+		cin>>first_player;
+		int position;
 
+		while(game.game_over() == false)
+		{
+			cout<<"Enter a position from 1-9: ";
+			cin>>position;
+			game.mark_board(position);
+			game.displey_board();
+		}
+		cout<<"Do you wnat to quit? enter N";
+		cin>>choice;
+	}
+	while(choice == 'N')
 	return 0;
 }
