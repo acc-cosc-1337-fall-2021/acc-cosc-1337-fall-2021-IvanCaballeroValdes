fb5be80878a37472c190c0cc4dded50db3aef2eb
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-11-11 21:47:34 -0600

    hw 4 corrections.

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -3,37 +3,31 @@
 #include<dna.h>
 using namespace std;
 //add function(s) code here
-int factorial()
+int factorial(int num)
 {
-    int num;
     long double factorial = 1.0;
-    cout<<"Enter a positive intiger: ";
-    cin>>num;
-    if(num<0)
-    cout<<"Error! Factorial of a negative number do not exist: ";
-    else {
-        for(int i = 1; i<= n; ++i){
+    
+    for(int i = 1; i<= num; ++i){
             factorial *= i;
         }
-        cout<<"Factorial of " <<n<< "=="<< factorial;
-    }
-    return 0; 
+    
+    return factorial; 
 }
 
-int gcd()
+int gcd(int num1, int num2)
 {
-    int num1;
-    cout<<"enter a number : ";
-    cin>>num1;
-    int num2;
-    cout<<"enter a number : ";
-    cin>>num2;
-    int gcd;
-    for (int i=1;i,<=num1&&i<=num2;i++) {
-        if(num1%i==0 && num2%i == 0) {
-            gcd=i;
+     while(num1 != num2)
+    {
+        if(num2 > num1) 
+        {
+            num1 = num1 + num2; // swap the values of the variables without temp variable
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+        } 
+        else 
+        {
+            num1 -= num2;
         }
     }
-    cout<<"the great common divisor is: "<<gcd<<endl;
-    return 0;
+    return num1;
 }
--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -5,10 +5,4 @@ using namespace std;
 Write prototype for function(s)
 */
 int factorial(int num);
-{
-    return num;
-}
 int gcd( int num1, int num2);
-{
-    return mum;
-}
